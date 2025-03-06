1407fc020509b8283880d4ec1d356d23dc74e36a
shriman <shrimansoft@gmail.com>
2025-03-06 06:34:13 +0530

    https://pytorch.org/docs/stable/notes/serialization.html#weights-only

--- seac/a2c.py
+++ seac/a2c.py
@@ -58,7 +58,7 @@ class A2C:
         torch.save(self.saveables, os.path.join(path, "models.pt"))
 
     def restore(self, path):
-        checkpoint = torch.load(os.path.join(path, "models.pt"))
+        checkpoint = torch.load(os.path.join(path, "models.pt"), weights_only=False)
         for k, v in self.saveables.items():
             v.load_state_dict(checkpoint[k].state_dict())
 
